a585e74bfc3d817f759db94a31a88a59dae42aa1
DilZam <dilyar.zamaliev@yandex.ru>
2026-03-02 16:50:26 +0300

    Реклама с картинкой

--- first_page.py
+++ first_page.py
@@ -37,5 +37,40 @@ def image_mars():
                 </html>'''
 
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="/static/img/mars.png" alt="здесь должна была быть картинка, но не нашлась" width="300">
+                    <div class="alert alert-primary" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-primary" role="alert" style='color: green; background-color: #98FB98;'>
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-primary" role="alert" style='color: blue; background-color: #7FFFD4;'>
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-primary" role="alert" style='color: #BDB76B; background-color: #F0E68C;'>
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-primary" role="alert" style='color: #C71585; background-color: #FFB6C1;'>
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
