d42717caf88ba0479762a023fd0e5ae7268ae237
DilZam <dilyar.zamaliev@yandex.ru>
2026-03-04 15:43:36 +0300

    Список профессий

--- first_page.py
+++ first_page.py
@@ -395,5 +395,9 @@ def carousel():
 def training(prof):
     return render_template('training.html', prof=prof.capitalize())
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list_temp.html', list=list)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
